eb1ce3d7779177790c49519bcfabbd53685e22f6
ma-mrl <marion.mrl@live.fr>
2024-07-09 15:00:56 +0100

    /api/minions routes

--- server/api.js
+++ server/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionRouter = require('./minion');
+apiRouter.use('/minions', minionRouter);
 
+const ideaRouter = require('./idea');
+apiRouter.use('/ideas', ideaRouter);
+
+const meetingRouter = require('./meeting');
+apiRouter.use('/meetings', meetingRouter);
 
 module.exports = apiRouter;
---
+++ server/minion.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const db = require("./db");
+const bodyParser = require("body-parser");
+
+const minionRouter = express.Router();
+minionRouter.use(bodyParser.json());
+
+let minions = db.getAllFromDatabase("minions");
+
+minionRouter.use("/:minionId", (req, res, next) => {
+  let minionId = req.params.minionId;
+  let minionIndex = minions.findIndex((obj) => obj.id === minionId);
+  if (minionIndex !== -1) {
+    req.minionIndex = minionIndex;
+    req.minionId = minionId;
+    next();
+  } else {
+    res.status(404).send("Minion not found!");
+  }
+});
+
+minionRouter.get("/", (req, res, next) => {
+  res.send(minions);
+});
+
+minionRouter.get("/:minionId", (req, res, next) => {
+  res.send(minions[req.minionIndex]);
+});
+
+minionRouter.post("/", (req, res, next) => {
+  const receivedMinion = req.body;
+  if (receivedMinion) {
+    db.addToDatabase("minions", receivedMinion);
+    res.status(201).send(receivedMinion);
+  } else {
+    res.status(400).send();
+  }
+});
+
+minionRouter.put("/:minionId", (req, res, next) => {
+  db.updateInstanceInDatabase("minions", req.body);
+  minions = db.getAllFromDatabase("minions");
+  res.send(minions[req.minionIndex]);
+});
+
+minionRouter.delete("/:minionId", (req, res, next) => {
+  db.deleteFromDatabasebyId("minions", req.params.minionId);
+  res.status(204).send();
+});
+
+module.exports = minionRouter;
